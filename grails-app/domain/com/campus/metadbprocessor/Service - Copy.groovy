96cf3451156b26d29612843f6308a9f343390186
Akter Hossain Sohag <akterhossain.dsi@gmail.com>
2015-05-06 15:52:34 +0600

    kkk

---
+++ grails-app/domain/com/campus/metadbprocessor/Service - Copy.groovy
@@ -0,0 +1,24 @@
+package com.campus.metadbprocessor
+
+class Service {
+
+    String loader
+    String name
+    String description
+    Integer overviewID
+    String clazz
+    String serviceParams = "{'text':1}"
+    Boolean isActive = false
+    Integer oca = 1
+
+    static mapping = {
+        version false
+        id column: "ServiceID"
+    }
+
+    static constraints = {
+        description(nullable: true)
+        overviewID(nullable: true)
+    }
+
+}
